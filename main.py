44f4104c5a6551f652db3a3e623527446cfe1029
rlxm7100 <seokwj@hotmail.com>
2022-08-28 23:25:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI() # 인스턴스 생성
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/") # get method로 '/'에 해당하는  생성
+def root():
+    return {'Hello':'World!'} 
